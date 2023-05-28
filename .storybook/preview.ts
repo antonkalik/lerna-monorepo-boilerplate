b14ae35b8ad4d5ac27a0a11d8339b47dd5403cd4
antonkalik <antonkalik@gmail.com>
2023-05-28 13:07:18 +0200

    default options of preview storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,14 @@ import type { Preview } from "@storybook/react";
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+    options: {
+      storySort: (a, b) => {
+        return a.title === b.title
+            ? 0
+            : a.id.localeCompare(b.id, { numeric: true });
+      },
+    },
+    layout: "fullscreen",
     controls: {
       matchers: {
         color: /(background|color)$/i,
